f226b846fe3b56c3f09879690fa4de55dcf27466
parth <parthprajapati467@gmail.com>
2025-02-04 10:22:20 -0500

    analysics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DashboardLayout from "./components/DashboardLayout";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <div className="h-screen">
       <DashboardLayout />
+      <Analytics />
     </div>
   );
 }
